422bab6ca219a59e304b6972227075b9e4e9c060
akkhadka <akkhadka@miu.edu>
2021-01-18 18:01:28 -0600

    alarm fix
    alarm fixes done with minute and second

--- lesson6/lab/alarm/js/alarm.js
+++ lesson6/lab/alarm/js/alarm.js
@@ -1,8 +1,16 @@
 window.onload = function () {
     const btnGo = document.getElementById('go');
-    btnGo.onclick = startSecCountDown;
+    btnGo.onclick = startAllCountDown;
     const txtSec = document.getElementById('sec');
+    const txtmin = document.getElementById('min');
     const container = document.getElementById('container');
+    function startAllCountDown() {
+        startSecCountDown();
+        //min
+        startCountDown(txtmin, 'veryawesome', 60000)
+        //sec
+
+    }
     function startSecCountDown() {
         container.className = '';
         let initSec = Number(txtSec.value);
@@ -11,12 +19,25 @@ window.onload = function () {
                 if (initSec === 0) {
                     clearInterval(intervalSec);
                     container.className = 'awesome';
+
+                    startMinCountDown();
                 }
                 else {
                     initSec -= 1;
                     txtSec.value = initSec;
                 }
             }, 1000)
+        } else {
+            startMinCountDown()
+        }
+    }
+    function startMinCountDown() {
+        let initMin = Number(txtmin.value);
+        if (initMin > 0) {
+            initMin -= 1;
+            txtmin.value = initMin;
+            txtSec.value = 60;
+            startSecCountDown();
         }
     }
 }
